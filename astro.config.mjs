a90a39c99ecddf55c0ee83b6770ad6920c89a753
Salih YALÇIN <yalcins@itu.edu.tr>
2024-11-14 14:04:46 +0300

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yourusername.github.io',
-  base: '/your-repo-name',
+  site: 'https://salihyalcin.github.io',
+  base: '/saliyalcin.github.io',
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
